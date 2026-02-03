2cf9b9ace9a6d42d10714c18f795caf40a436d17
sergioomoolina <sergiomolinacuenca0@gmail.com>
2026-02-03 20:26:34 +0100

    Added: Exercise0.java and Exercise1.java

---
+++ src/fundamentals/Day01_Variables/Exercise0.java
@@ -0,0 +1,5 @@
+public class Exercise0{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/fundamentals/Day01_Variables/Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1{
+    public static void main(String[] args){
+        String name = "Sergio";
+        int age = 18;
+        boolean studyingDAMDAW = true;
+        float height = 1.75f;
+        System.out.println("Name: " + name + "; Age: " + age + "; Studying DAM/DAW: " + studyingDAMDAW + "; Height: " + height + "m");
+    }
+}
