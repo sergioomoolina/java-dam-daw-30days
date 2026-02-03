7ed1edddf6c02d55952983e81e10d0282859ba45
sergioomoolina <sergiomolinacuenca0@gmail.com>
2026-02-03 21:07:17 +0100

    Added: Exercise 2

--- src/fundamentals/Day01_Variables/Exercise2.java
+++ src/fundamentals/Day01_Variables/Exercise2.java
@@ -1,3 +1,9 @@
 public class Exercise2 {
-    
+    public static void main(String[] args){
+        int Age = 18;
+        double Height = 1.76;
+        char Initial = 'S';
+        boolean Adult = true;
+        System.out.println("Age: " + Age + "; Height: " + Height + "m; Initial: " + Initial + "; Adult: " + Adult);
+    }
 }
